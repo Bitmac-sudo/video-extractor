4ddb765df4cce855cd720eebca48b540a7c907ec
Bitmac Tv <aabualez66@gmail.com>
2025-12-23 23:14:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Endpoint لعرض روابط الفيديوهات
+app.get('/videos', (req, res) => {
+  res.json({
+    video1: process.env.VIDEO1,
+    video2: process.env.VIDEO2,
+    video3: process.env.VIDEO3
+  });
+});
+
+// صفحة رئيسية للتأكد من تشغيل السيرفر
 app.get('/', (req, res) => {
   res.send('تم تشغيل السيرفر الخاص بك بنجاح على Render!');
 });
